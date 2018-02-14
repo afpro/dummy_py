e15f6faa25247d064ddee65c100dea6ead14f498
afpro <admin@afpro.net>
2018-02-14 14:41:28 +0800

    to v1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dummy_py',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     url='https://github.com/afpro/dummy_py',
     license='Apache',
