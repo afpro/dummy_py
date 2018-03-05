d9b6a6d0a09eb50d21cb1a8d41bdda13e9d544af
afpro <yanshuang.liu@kikatech.com>
2018-03-05 17:01:34 +0800

    mask attention with logits transform, avoid float problem

--- dummy_py/tf_parts/g_attn.py
+++ dummy_py/tf_parts/g_attn.py
@@ -129,16 +129,19 @@ def multi_head_attention(q: 'tf_input',
                 kh = tf.concat(tf.split(kh, n_head, axis=-1), axis=0)
                 vh = tf.concat(tf.split(vh, n_head, axis=-1), axis=0)
             attn = tf.matmul(qh, tf.transpose(kh, perm=[0, 2, 1])) / (d_model ** 0.5)
-            attn = tf.nn.softmax(attn)
             if attn_mask is not None:
-                attn = attn + 1
+                # 1. make sure min value in logits is zero
+                attn -= tf.reduce_min(attn, axis=-1, keep_dims=True)
+                # 2. multiply by attention mask, masked value is zero (as min value)
                 if n_head > 1:
                     attn = tf.split(attn, n_head, axis=0)
                     attn = [_ * attn_mask for _ in attn]
                     attn = tf.concat(attn, axis=0)
                 else:
                     attn = attn * attn_mask
-                attn = attn / (tf.reduce_sum(attn, axis=-1, keep_dims=True) + 1e-6)
+                # 3. minus masked value by -1e9, after softmax, almost zero
+                attn += (attn_mask - 1) * 1e9
+            attn = tf.nn.softmax(attn)
             value = tf.matmul(attn, vh)
             if n_head > 1:
                 value = tf.concat(tf.split(value, n_head, axis=0), axis=-1)
